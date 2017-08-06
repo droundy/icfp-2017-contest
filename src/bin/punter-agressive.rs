295ceaafdb8b43ba4adb659252734ccb7ee6bd88
David Roundy <roundyd@physics.oregonstate.edu>
2017-08-06 14:05:53 -0700

    tweak agressive guy just a tad

--- src/bin/punter-agressive.rs
+++ src/bin/punter-agressive.rs
@@ -3,9 +3,9 @@ use punter::StateRater::*;
 
 fn main() {
     punter::main_helper(
-        punter::Optimizer::Greedy(Score + 0.0001*BottleNecks + 1000.0*AllMines)
-            + punter::Optimizer::Greedy((Score + 0.0001*BottleNecks + 1000.0*AllMines)/5.0)
-            + punter::Optimizer::Greedy((Score + 0.0001*BottleNecks + 1000.0*AllMines)/5.0/5.0)
+        punter::Optimizer::Greedy(Score + 0.1*BottleNecks + 100.0*AllMines)
+            + punter::Optimizer::Greedy((Score + 0.1*BottleNecks + 100.0*AllMines)/5.0)
+            + punter::Optimizer::Greedy((Score + 0.1*BottleNecks + 100.0*AllMines)/5.0/5.0)
     );
 }
 
