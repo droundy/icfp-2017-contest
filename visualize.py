e0efb5a80ff22dae32532703f616f436711bf43e
Chris May <maychr@oregonstate.edu>
2017-08-05 19:43:15 -0700

    Added some more maps to duke it out

--- visualize.py
+++ visualize.py
@@ -3,36 +3,7 @@ import matplotlib.pyplot as plt
 import puntertools as pt
 from rwjson import readMessage,writeMessage,readJson
 
-
-with open('examples/gameplay0.txt') as f:
-    handshake = readMessage(f)
-    messageIn = readMessage(f)
-    setup = pt.map_to_nice(messageIn['move']['state']['map'])
-    R = len(setup['riverdata'])
-
-
-plt.figure()
-for turn in range(0,5):
-    gameState = ['examples/gameplay'+str(turn)+'.txt']
-
-    
-    # Currently the map and the gamestate are not coupled.
-    # By that I mean that the state id's which are specified in the file gameplay
-    # are the same as the id's for maps/sample.json only
-    
-    with open(gameState[0]) as f:
-        handshake = readMessage(f)
-        game = readMessage(f)
-        pt.update_nice(setup,game['move']['moves'])
-    
-    punter = game['move']['moves'][0]['claim']['punter']
-#    print "punter", punter
-    farg = ['-y','-m','-m','-y','-w']
-    turns = 1
-    
-    ##### We want to have an arbitrary number of plays
-    ##### When player n makes a choice, that color gets put on plot
-        
+def visualize_board(setup):
     for i in range(len(setup['riverdata'])):
         if setup['riverdata'][i]['claimed'] == None:
             riverid = setup['riverdata'][i]['id']
@@ -51,4 +22,25 @@ for turn in range(0,5):
     for k in range(len(setup['mines'])):
         mineLoc = setup['mines'][k]
         plt.plot(setup['sites'][mineLoc]['x'],setup['sites'][mineLoc]['y'],'-ro')
+        
+with open('examples/gameplay0.txt') as f:
+    handshake = readMessage(f)
+    messageIn = readMessage(f)
+    setup = pt.map_to_nice(messageIn['move']['state']['map'])
+    R = len(setup['riverdata'])
+
+farg = ['-y','-m','-m','-y','-w']
+    
+for turn in range(R):
+    gameState = ['examples/gameplay'+str(turn)+'.txt']
+    
+    with open(gameState[0]) as f:
+        handshake = readMessage(f)
+        game = readMessage(f)
+        pt.update_nice(setup,game['move']['moves'])
+        punter = game['move']['moves'][0]['claim']['punter']
+    visualize_board(setup)
+
 plt.show()
+
+
