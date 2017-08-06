d465bbb3edffc1fedb332340af55640a869b2a2f
garrettjepson <jepson23g@gmail.com>
2017-08-05 17:01:05 -0700

    added mine init. optimizer

--- src/optimize.rs
+++ src/optimize.rs
@@ -3,6 +3,8 @@ use super::*;
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub enum Optimizer {
     Random,
+    InitialMine,
+
 }
 
 impl Optimizer {
@@ -25,7 +27,29 @@ impl Optimizer {
                     }
                 }
             },
+            &Optimizer::InitialMine => {
+                let available: Vec<_> = state.riverdata.iter()
+                    .filter(|r| r.claimed.is_none()).cloned().collect();
+                let minerivers: Vec<RiverData> = state.map.mines.iter()
+                    .flat_map(|m| state.rivermap[m].values().map(|r| state.riverdata[r.0].clone()))
+                    .filter(|r| r.claimed.is_none())
+                    .collect();
+                if minerivers.len() > 0 {
+                    if let Ok(mut plan) = bestlaidplan.lock() {
+                        if plan.value < 0.0 {
+                            *plan = Plan {
+                                value: 1.0,
+                                river: available[0].id,
+                                why: format!("get to mine"),
+                            };
+                        }
+                    }
+                } else {
+                    Optimizer::Random.optimize(state, bestlaidplan);
+                }        
+            },
         }
     }
 }
+
 
